7ec4035ca45b461371eb853d1f0a6c7b0ca51e97
keepgoing <jp956956@gmail.com>
2020-10-22 19:57:51 +0800

    增加ubike資料連結

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const bot = linebot({
 })
 
 bot.on('message', async event =>{
+    // 新北市ubike交通資料
+    const traffic  = await axios.get('https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json?page=0&size=1000')
+    // 職訓局周邊食衣住
     // 抓取本地檔案不需要await，不然會發生錯誤
     const response = axios.get('./test.json')
-    let reply = '123'
+    let reply = ''
     try {
-        if(event.message.text === '讀書'){
-            event.reply(reply)
-        }       
+        for(let data of traffic.data)
+{            if(event.message.text === data.sno){
+                reply = data.sarea
+                event.reply(reply)
+            } }      
     } catch (error) {
         event.reply('錯誤')
     }
